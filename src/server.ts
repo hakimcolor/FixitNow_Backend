04f37cf914c6e95b745bc8d087df3e006d79df09
hakimcolor <hakimcolor777@gmail.com>
2026-07-30 12:58:32 +0600

    fix server.ts and  and fixing the seed.ts

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,6 @@
-import app from './app';
-import config from './config';
-import { prisma } from './lib/prisma';
+import app from './app.js';
+import config from './config/index.js';
 
-const PORT = config.port;
-
-
-async function main() {
-  try {
-    await prisma.$connect();
-    console.log('connected to the database successfully');
-
-    app.listen(PORT, () => {
-      console.log(`server is runing port on ${PORT}`);
-    });
-  } catch (error) {
-    console.error('error ', error);
-    await prisma.$disconnect();
-    process.exit(1);
-  }
-}
-main();
+app.listen(config.port, () => {
+  console.log(`Server running on http://localhost:${config.port}/api/health`);
+});
